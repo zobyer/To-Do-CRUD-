59ef4af2a0d31ce9e7e0d5205162d9d3b03aa455
unknown <ahmzobyer@gmail.com>
2021-04-18 17:35:37 +0600

    Create done

---
+++ resources/js/components/App.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="toDocontainer">
+  <div class="toDo">
+    <h2>To DO App</h2>
+    <input-item />
+  </div>
+  <item-list />
+</div>
+</template>
+
+<script>
+import inputItem from './inputItem.vue'
+import ItemList from './itemList.vue'
+
+export default {
+  components: { inputItem, ItemList },
+  
+}
+</script>
+
+<style>
+.toDocontainer{
+  width: 350px;
+  margin: auto;
+}
+
+.toDo{
+  background: #e6e6e6;
+  padding: 10px;
+  text-align: center;
+}
+
+</style>
---
+++ resources/js/components/inputItem.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="insert">
+    <input type="text" v-model="item.name" placeholder="Enter anything...">
+    <button @click="insert_in" :class="[item.name ? 'active': 'inactive', 'plus' ]">Submit</button>
+</div>
+</template>
+
+<script>
+export default {
+    data: function(){
+        return{
+            item: {
+                name:''
+            }
+        }
+    },
+    methods:{
+        insert_in(){
+            if ( this.item.name != ''){
+                axios.post('api/item/store',{
+                item: this.item
+                })
+                .then( response =>{
+                    if( response.status == 201){
+                        this.item.name =''
+                        console.log("Item Inserted Succesfully")
+                    }
+                })
+                .catch( error =>{
+                    console.log("Item Inserting error")
+                })
+            }else{
+                console.warn('insert something')
+            }
+        }
+    }
+}
+</script>
+
+<style>
+.insert{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.insert input{
+    background: #f7f7f7;
+    border: 0px;
+    border-radius: 10px;
+    outline: none;
+    padding: 5px;
+    margin-right: 10px;
+    width: 100%;
+}
+
+.plus{
+    height: 25px;
+    border: none;
+    color: white;
+
+}
+
+.active{
+    background: green;
+    color: white;
+    border: none;
+    
+}
+.inactive{
+    background: tomato;
+}
+
+
+</style>
